b99e8dc1d3b08b4fa4b0b9553187f43969a0c091
mhf24 <96777182+mhf24@users.noreply.github.com>
2022-01-05 15:50:49 +0200

    issue #4
    Create a new function validate to validate the question with the right format

--- Quiz/Question.cs
+++ Quiz/Question.cs
@@ -27,14 +27,50 @@ namespace Bdeir.Quizzer
         protected Answer _correctAnswer;
         public IAnswer CorrectAnswer => Answers.First(p => p.Correct);
         
+        public bool validate(string prompt) {
+            bool star, minus, equals;
+            star = minus = equals = false;
+            string[] prompt_questions = prompt.Split(Environment.NewLine,StringSplitOptions.RemoveEmptyEntries);
 
+            for (int i = 0; i < prompt_question.Length; i++) {
+                foreach (string prompt_question in prompt_questions){
+                    switch (prompt_question[0]) 
+                    {
+                        case '*':
+                        star = true;
+                        break;
+                        case '-':
+                        minus = true;
+                        break;
+
+                        case '=':
+                        equals = true;
+                        break;
+
+                        default:
+                        return false;
+                        break;
+                    }
+                }
+            }
+            if (star && minus && equals){
+              return true;
+            } else {
+              return false;
+            }
+        }
+        
         #region Constructors and Static Factory Methods
         internal static int SeqNum = 0; // allow Question to reset it each time a Question is created , issue #7
         protected Question(string prompt)
         {
             if(prompt.Length > 300) {
                 throw new IndexOutOfRangeException("Exceeded 300 characters!!");
-            } 
+            }
+            
+            if(!validate( prompt )) {
+                throw new InvalidOperationException("Question Format Is Wrong");
+            }
             Prompt = prompt;
             Answers = new List<Answer>();
             SequenceNumber = SeqNum++;
